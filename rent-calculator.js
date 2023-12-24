16a33ab9b29ac5e7999d16ab3dcb27757b3589b4
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-24 14:01:21 -0300

    Create rent-calculator.js

---
+++ rent-calculator.js
@@ -0,0 +1,7 @@
+/*
+ 14) The car rental company needs your help to charge for its services. 
+ Write a program that asks for the number of kilometers traveled by a 
+ rented car and the number of days it was rented. 
+ Calculate the total price to pay, 
+ knowing that the car costs 90 USD per day and 0.20USD per kilometer traveled.
+*/
