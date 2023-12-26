c8d8094f5e275caba3b520232ad31d5e5679b155
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-26 19:29:41 -0300

    Create know-your-salary.js

---
+++ know-your-salary.js
@@ -0,0 +1,6 @@
+/* 
+  15) Create a program that reads the number of days worked in a month and shows
+  the salary of an employee, knowing that they work 8 hours a day and earn R$25
+  per hour worked.
+*/
+
