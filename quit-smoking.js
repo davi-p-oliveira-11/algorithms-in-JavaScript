f5293eee0b94b9f1982b6487da4cd449556dcc07
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-26 19:43:10 -0300

    Update quit-smoking.js

--- quit-smoking.js
+++ quit-smoking.js
@@ -4,3 +4,23 @@ Ask for the number of cigarettes smoked per day and how many years they have bee
 Consider that a smoker loses 10 minutes of life for each cigarette. Calculate
 how many days of life a smoker will lose and display the total in days.
 */
+
+const prompt = require('prompt-sync')();
+const readline = require('readline-sync');
+
+function quitSmoking () {
+  
+  const cigarsPerDay = parseInt(prompt('Quantos cigarros voce fuma por dia ? '));
+  const yearsSmoking = parseInt(prompt('Há quantos anos você fuma ? '));
+
+  const totalOfCigarretes = (cigarsPerDay * 365) * yearsSmoking; // 1 year = 365 days
+
+  const minutesLost = totalOfCigarretes * 10; // Time lost for smoking 1 cigarette.
+
+  const daysLost =  minutesLost / 1440; // Minutes that fit in one day.
+
+  console.log(`Você perdeu já ${daysLost.toFixed(1)} dias de vida`);
+}
+
+quitSmoking();
+readline.question('Press Enter to Exit');
