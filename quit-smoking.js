0cf9a8d7a54e5ef5759e403d539941ca2f1895f9
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-26 19:42:47 -0300

    Create quit-smoking.js

---
+++ quit-smoking.js
@@ -0,0 +1,6 @@
+/*
+16) [CHALLENGE] Write a program to calculate the reduction in the lifespan of a smoker.
+Ask for the number of cigarettes smoked per day and how many years they have been smoking.
+Consider that a smoker loses 10 minutes of life for each cigarette. Calculate
+how many days of life a smoker will lose and display the total in days.
+*/
