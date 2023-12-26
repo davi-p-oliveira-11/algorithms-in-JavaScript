3f38a5777b19dac869bb09ff29d9bf58c64597f5
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-26 16:43:57 -0300

    Update area-calculator.js

--- area-calculator.js
+++ area-calculator.js
@@ -4,3 +4,21 @@
  and the amount of paint needed for the job, 
  considering that each liter of paint covers an area of 2 square meters.
 */
+
+const prompt = require('prompt-sync')();
+const readline = require('readline-sync');
+
+function areaCalculator() {
+ 
+  const width = parseFloat(prompt('What is the width of the wall? (Enter a value in meters)' ));
+  const height  = parseFloat(prompt('What is the height of the wall? (Enter a value in meters) '));
+
+  const area = width * height;
+  const paintNeeded = area * 0.5;
+
+  console.log(`The area of the wall to be painted is ${area}m²,
+  and the amount of paint needed to paint the wall is ${paintNeeded} liters.`);
+}
+
+areaCalculator();
+readline.question();
