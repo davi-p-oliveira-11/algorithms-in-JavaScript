03876b49b959ada64b541d529669820ee8d0c19a
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-27 19:34:02 -0300

    Update delta-calculator.js

--- delta-calculator.js
+++ delta-calculator.js
@@ -3,21 +3,19 @@
  from a quadratic equation and displays the value of Delta.
 */
 
-const prompt = require('prompt-sync')();
-const readline = require('readline-sync');
+const prompt = require("prompt-sync")();
+const readline = require("readline-sync");
 
-function deltaCalc () {
-  
-  const valueA = parseInt(prompt('Enter the value of A '));
-  const valueB = parseInt(prompt('Enter the value of B '));
-  const valueC = parseInt(prompt('Enter the value of C '));
-  
+function deltaCalc() {
+  const valueA = parseInt(prompt("Enter the value of A "));
+  const valueB = parseInt(prompt("Enter the value of B "));
+  const valueC = parseInt(prompt("Enter the value of C "));
+
   /* Bhaskara's formula:  delta = b² - 4ac */
-  const deltaValue = (valueB ** 2) - 4 * (valueA * valueC);
+  const deltaValue = valueB ** 2 - 4 * (valueA * valueC);
 
   console.log(`The value of delta is equal to: ${deltaValue}`);
-  
 }
 
 deltaCalc();
-readline.question();
+readline.question('Press Enter to Exit ...');
