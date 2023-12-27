a85224307984312f6186b2a6286f3d25b7b59eb9
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-27 19:27:26 -0300

    Update code format

--- 15-raise.js
+++ 15-raise.js
@@ -3,18 +3,17 @@
  calculates, and displays their new salary with a 15% increase."
 */
 
-const prompt = require('prompt-sync')();
-const readline = require('readline-sync');
+const prompt = require("prompt-sync")();
+const readline = require("readline-sync");
 
 function raiseSalary() {
-  
-  const salary = parseFloat(prompt('Qual o valor do seu salário ?'));
- 
+  const salary = parseFloat(prompt("Qual o valor do seu salário ?"));
+
   const raiseAmount = salary * 0.15;
   const newSalary = salary + raiseAmount;
- 
+
   console.log(`Your new salary with a 15% raise is equivalent to ${newSalary}`);
 }
 
 raiseSalary();
-readline.question('Press Enter to Exit ... ');
+readline.question("Press Enter to Exit ... ");
