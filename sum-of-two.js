a5a21f443edca810f6e0b57ab9122fe7e61e68ec
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-27 19:50:46 -0300

    Update sum-of-two.js

--- sum-of-two.js
+++ sum-of-two.js
@@ -6,18 +6,17 @@ Enter another value: 5
 The sum of 8 and 5 is equal to 13.
 */
 
-const prompt = require('prompt-sync')();
-const readline = require('readline-sync');
+const prompt = require("prompt-sync")();
+const readline = require("readline-sync");
 
-function sum () {
-
-  const num1 = parseInt(prompt('Enter a number: ' ));
-  const num2 = parseInt(prompt('Enter another number: ' ));
+function sum() {
+  const num1 = parseInt(prompt("Enter a number: "));
+  const num2 = parseInt(prompt("Enter another number: "));
 
   const sum = num1 + num2;
-  
+
   console.log(`The sum between  ${num1} and ${num2} is equal to ${sum}`);
 }
 
 sum();
-readline.question();
+readline.question("Press Enter to Exit ... ");
