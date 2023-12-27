f41c364979d09b59054d39f656baace21a587b13
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-27 19:46:03 -0300

    Update code format

--- measure-converter.js
+++ measure-converter.js
@@ -1,5 +1,4 @@
 /* 
-
 8) Develop a program that reads a distance in meters and shows the
 corresponding values in other units.
 Example:
@@ -11,23 +10,21 @@ The distance of 185.72m corresponds to:
 1857.2dm
 18572.0cm
 185720.0mm
-
 */
 
-const prompt = require('prompt-sync')();
-const readline = require('readline-sync');
+const prompt = require("prompt-sync")();
+const readline = require("readline-sync");
 
-function measureCalc () {
-  
-  const meters = parseFloat(prompt('Digite uma distancia em metros '));
+function measureCalc() {
+  const meters = parseFloat(prompt("Digite uma distancia em metros "));
 
-  const kilometers  =  meters / 1000;
-  const hectometers =  meters /  100;
-  const decameters  =  meters /   10;
-  const decimeters  =  meters *   10;
-  const centimeters =  meters *  100;
-  const millimeters =  meters * 1000;
-  
+  const kilometers = meters / 1000;
+  const hectometers = meters / 100;
+  const decameters = meters / 10;
+  const decimeters = meters * 10;
+  const centimeters = meters * 100;
+  const millimeters = meters * 1000;
+
   console.log(`A distancia de ${meters.toFixed(2)}m corresponde a:
   ${kilometers.toFixed(5)} Km
   ${hectometers.toFixed(4)}Hm
@@ -35,8 +32,7 @@ function measureCalc () {
   ${decimeters.toFixed(1)} dm
   ${centimeters.toFixed(1)}cm
   ${millimeters.toFixed(1)}mm`);
-
 }
 
 measureCalc();
-readline.question();
+readline.question('Press Enter to Exit ... ');
