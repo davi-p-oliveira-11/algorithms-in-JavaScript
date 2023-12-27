cfe180d99102d4ad1a6a673f82e4132794861039
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-27 19:29:08 -0300

    Update average-of-two.js

--- average-of-two.js
+++ average-of-two.js
@@ -6,18 +6,18 @@ Grade 2: 8.5
 The average between 4.5 and 8.5 is equal to 6.5.
 */
 
-const prompt = require('prompt-sync')();
-const readline = require('readline-sync');
+const prompt = require("prompt-sync")();
+const readline = require("readline-sync");
 
-function averageOfTwo () {
-  
-  const grade1 = parseFloat(prompt('Enter the first grade '));
-  const grade2 = parseFloat(prompt('Enter the second grade '));
+function averageOfTwo() {
+  const grade1 = parseFloat(prompt("Enter the first grade "));
+  const grade2 = parseFloat(prompt("Enter the second grade "));
 
   const average = (grade1 + grade2) / 2;
 
-  console.log(`The average between ${grade1} and ${grade2} is equal to ${average.toFixed(1)}`);
-
+  console.log(
+    `The average between ${grade1} and ${grade2} is equal to ${average.toFixed(1)}`
+  );
 }
 
 averageOfTwo();
