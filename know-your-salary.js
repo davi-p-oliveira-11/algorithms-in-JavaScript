16e78e4b6d0e3c8e0de2a76efc8ec911168ff6b3
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-27 19:44:29 -0300

    Update the code formatting

--- know-your-salary.js
+++ know-your-salary.js
@@ -4,19 +4,19 @@
   per hour worked.
 */
 
-const prompt = require('prompt-sync')();
-const readline = require('readline-sync');
+const prompt = require("prompt-sync")();
+const readline = require("readline-sync");
 
-function knowSalary () {
-
-  const daysWorked = parseInt(prompt('How many days did you work last month ? '));
+function knowSalary() {
+  const daysWorked = parseInt(
+    prompt("How many days did you work last month ? ")
+  );
   const dayOfWork = 8 * 25;
   const employeeSalary = daysWorked * dayOfWork;
-        
+
   console.log(`The employee worked for ${daysWorked} days and will 
-  receive a salary of ${employeeSalary} reais.`);
+  receive a salary of ${employeeSalary} USD.`);
 }
 
 knowSalary();
-readline.question('Press Enter to Exit');
-
+readline.question("Press Enter to Exit ... ");
