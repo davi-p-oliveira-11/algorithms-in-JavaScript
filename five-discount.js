b58103ccbe417fa3050620052b717fb959672524
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-27 19:42:58 -0300

    inserted the challenge solution

--- five-discount.js
+++ five-discount.js
@@ -2,3 +2,20 @@
 12) Create a program that reads the price of a product, 
 calculates, and displays its PROMOTIONAL PRICE with a 5% discount.
 */
+
+const prompt = require("prompt-sync")();
+const readline = require("readline-sync");
+
+function fiveDiscount() {
+  const productPrice = parseFloat(prompt("Qual o valor do produto ? "));
+
+  const discount = productPrice * 0.05;
+  const finalPrice = productPrice - discount;
+
+  console.log(`O preço do produto é: ${productPrice}, 
+  e o preço promocional do produto é ${finalPrice}`);
+}
+
+fiveDiscount();
+readline.question("Press Enter to Exit ...");
+
