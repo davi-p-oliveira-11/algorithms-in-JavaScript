4b1b66116cc8026982707ec95fe5c5fe89135596
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-28 15:49:00 -0300

    Create enlistment-check.js

---
+++ enlistment-check.js
@@ -0,0 +1,8 @@
+/*
+22) Write a program that reads a boy's year of birth and shows his
+status in relation to military enlistment.
+- If he is under 18 years old, show how many years are left until
+enlistment.
+- If he is already over 18 years old, show how many years have passed since
+enlistment.
+*/
