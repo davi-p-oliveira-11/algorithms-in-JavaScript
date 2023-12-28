a4a5bd30a57562fa9008c6436486f9b3079eaa3d
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-28 16:23:03 -0300

    Create discount-calculator.js

---
+++ discount-calculator.js
@@ -0,0 +1,7 @@
+/*
+23) In an exclusive promotion for Women's Day, a store wants to offer discounts
+to everyone, but especially to women. Create a program that reads the name,
+gender, and the value of the customer's purchases, then calculates the discounted price. Knowing that:
+- Men get a 5% discount
+- Women get a 13% discount
+*/
