bdd6a48e0aaa291cc974c9fde06ecdbc58cd4e00
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-28 16:45:32 -0300

    Update number-comparison.js

--- number-comparison.js
+++ number-comparison.js
@@ -8,3 +8,21 @@ na tela uma das mensagens abaixo:
 
 const prompt = require("prompt-sync")();
 const readline = require("readline-sync");
+
+function numberComparison() {
+  const num1 = parseInt(prompt("Enter a number "));
+  const num2 = parseInt(prompt("Enter another number "));
+
+  if (num1 > num2) {
+    console.log(`The first value is greater.`);
+    readline.question("Press Enter to Exit ... ");
+  } else if (num1 < num2) {
+    console.log(`The second value is greater.`);
+    readline.question("Press Enter to Exit ... ");
+  } else if (num1 === num2) {
+    console.log(`There is no greater value, both are equal.`);
+    readline.question("Press Enter to Exit ... ");
+  }
+}
+
+numberComparison();
