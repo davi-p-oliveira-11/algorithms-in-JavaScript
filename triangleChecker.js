560b896735772ffa2522db5412973e7fc56c58b8
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-01 21:43:05 -0300

    logic structure

--- triangleChecker.js
+++ triangleChecker.js
@@ -4,3 +4,14 @@ Analyze their lengths and determine if it is possible to form a triangle with th
 Mathematically, for three segments to form a triangle, 
 the length of each side must be less than the sum of the other two.
 */
+
+const prompt = require('prompt-sync')();
+const readline = require('readline-sync');
+
+function checkTrianglePossibility () {
+  
+}
+
+checkTrianglePossibility();
+readline.question('Press Enter to Exit ... ')
+
