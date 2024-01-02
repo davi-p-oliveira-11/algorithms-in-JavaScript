7bc8ad1ae98f1ce6f59e05eb3f5665ddd2d923c7
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-02 11:24:38 -0300

    Create classify-terrain.js

---
+++ classify-terrain.js
@@ -0,0 +1,8 @@
+/*
+ 28)Create a program that reads the width and length of a rectangular terrain, 
+ calculating and displaying its area in square meters. 
+ The program should also show the classification of this terrain, according to the list below:
+Below 100m² = POPULAR LAND
+Between 100m² and 500m² = MASTER LAND
+Above 500m² = VIP LAND
+*/
