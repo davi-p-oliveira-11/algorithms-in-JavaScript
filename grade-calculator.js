b2866f74776e5773cffbf25aab4810bf1bff985b
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-02 10:29:21 -0300

    Create grade-calculator.js

---
+++ grade-calculator.js
@@ -0,0 +1,7 @@
+/*
+ 27) Create a program that reads two grades of a student and calculates their average, 
+ displaying a message at the end according to the achieved average:
+Average up to 4.9: FAIL
+Average between 5.0 and 6.9: RECOVERY
+Average 7.0 or higher: APPROVED
+*/
