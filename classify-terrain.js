18f1837cc32ba2faf3fdd273b6edc1ad13bf72e4
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-02 11:25:04 -0300

    Update classify-terrain.js

--- classify-terrain.js
+++ classify-terrain.js
@@ -6,3 +6,30 @@ Below 100m² = POPULAR LAND
 Between 100m² and 500m² = MASTER LAND
 Above 500m² = VIP LAND
 */
+
+const prompt = require("prompt-sync")();
+const readline = require("readline-sync");
+
+function classifyTerrain() {
+  const width = parseFloat(prompt("What is the width of the terrain ? "));
+  const length = parseFloat(prompt("What is the length of the terrain? "));
+
+  const area = width * length;
+
+  if (area < 100) {
+    console.log(
+      `The terrain measures ${area.toFixed(1)}m², so it is a Popular Terrain`
+    );
+    readline.question("Press Enter to Exit ... ");
+  } else if (area >= 100 && area <= 500) {
+    console.log(
+      `The terrain measures ${area.toFixed(1)}m², so it is a Master Terrain`
+    );
+    readline.question("Press Enter to Exit ... ");
+  } else {
+    console.log(`The terrain measures ${area.toFixed(1)}m², so it is a VIP Terrain.`);
+    readline.question("Press Enter to Exit ... ");
+  }
+}
+
+classifyTerrain();
