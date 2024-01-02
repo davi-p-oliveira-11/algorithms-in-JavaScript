f60c45cd7121e8983b26bac8ca12da7a96d94a5a
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-02 11:48:54 -0300

    Create salary-adjustment-calc.js

---
+++ salary-adjustment-calc.js
@@ -0,0 +1,7 @@
+/*
+ 29) Develop a program that reads an employee's name, salary, number of years 
+ they have been working for the company, and displays their new salary adjusted according to the following table:
+Up to 3 years at the company: 3% increase
+Between 3 and 10 years: 12.5% increase
+10 years or more: 20% increase
+*/
