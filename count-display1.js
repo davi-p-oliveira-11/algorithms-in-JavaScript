148385c51e997537af92a2d307ade27a1fadea46
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-03 14:49:43 -0300

    Update count-display1.js

--- count-display1.js
+++ count-display1.js
@@ -2,3 +2,19 @@
 38) Write a program that displays the following countdown on the screen:
 6 7 8 9 10 11 Done!
 */
+
+const readline = require("readline-sync");
+
+function displayCountDown() {
+  let counter = 6;
+
+  while (counter <= 11) {
+    console.log(counter);
+    counter++;
+  }
+
+  console.log("Done !");
+  readline.question('Press Enter to Exit ... ');
+}
+
+displayCountDown();
