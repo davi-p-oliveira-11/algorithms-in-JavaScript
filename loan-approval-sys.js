6cb372bd5de8514a10cf043dc78de390e3a3a2e0
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-03 13:46:08 -0300

    Create loan-approval-sys.js

---
+++ loan-approval-sys.js
@@ -0,0 +1,6 @@
+/*
+ 33) Write a program to approve or deny a bank loan for the purchase of a house. 
+ The program will ask for the value of the house, the buyer's salary, 
+ and how many years they will be paying. Calculate the monthly installment amount, 
+ knowing that it cannot exceed 30% of the salary, or else the loan will be denied.
+*/
