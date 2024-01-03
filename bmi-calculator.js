250334db25793c128d50f483fd0b1a5fc20c2f66
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-03 14:42:47 -0300

    Update bmi-calculator.js

--- bmi-calculator.js
+++ bmi-calculator.js
@@ -8,4 +8,34 @@ between 25 and 30: Overweight
 between 30 and 40: Obesity
 above 40: Morbid obesity
 Note: BMI is calculated by the expression weight/height² (weight divided by the square of height)
-*/d
+*/
+
+const prompt = require("prompt-sync")();
+const readline = require("readline-sync");
+
+function bmiCalc() {
+  const weight = parseFloat(prompt("Enter your weight in kilograms "));
+  const height = parseFloat(prompt("Enter your height in meters "));
+
+  const bmi = weight / height ** 2;
+
+  if (bmi < 18.5) {
+    console.log(`Your BMI is ${bmi.toFixed(2)} ,you are underweight.`);
+    readline.question("Press Enter to Exit ... ");
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    console.log(`Your BMI is ${bmi.toFixed(2)} ,you are at the ideal weight.`);
+    readline.question("Press Enter to Exit ... ");
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    console.log(`Your BMI is ${bmi.toFixed(2)} ,you are overweight`);
+    readline.question("Press Enter to Exit ... ");
+  } else if (bmi >= 30 && bmi <= 39.9) {
+    console.log(`Your BMI is ${bmi.toFixed(2)} ,you have obesity.`);
+    readline.question("Press Enter to Exit ... ");
+  } else if (bmi > 40) {
+    console.log(`Your BMI is ${bmi.toFixed(2)} you have morbid obesity.`);
+    readline.question("Press Enter to Exit ... ");
+  }
+}
+
+bmiCalc();
+
