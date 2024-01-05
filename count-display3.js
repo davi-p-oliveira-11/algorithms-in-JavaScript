18eb98227d2de8e115ba43b0027d701465464b89
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-04 23:34:48 -0300

    Update count-display3.js

--- count-display3.js
+++ count-display3.js
@@ -2,3 +2,15 @@
  40) Create an application that displays the following countdown on the screen:
 0 3 6 9 12 15 18 Done !
 */
+
+const readline = require('readline-sync');
+
+let counter = 0;
+
+while (counter <= 18) {
+  console.log(counter);
+  counter +=3;
+}
+
+console.log('Done !');
+readline.question();
