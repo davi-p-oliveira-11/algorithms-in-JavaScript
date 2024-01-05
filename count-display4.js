6c02cc0f62b51e1f548c8c9892916df6c76d7600
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-05 01:23:36 -0300

    Update count-display4.js

--- count-display4.js
+++ count-display4.js
@@ -3,14 +3,18 @@
 100 95 90 85 80 ... 0 Done !
 */
 
-const readline = require('readline-sync');
+const readline = require("readline-sync");
 
-let counter = 100;
+function displayCountDown() {
+  let counter = 100;
 
-while (counter >= 0){
-  console.log(counter);
-  counter -= 5;
+  while (counter >= 0) {
+    console.log(counter);
+    counter -= 5;
+  }
+
+  console.log("Done !");
+  readline.question("Press Enter to Exit ... ");
 }
 
-console.log('Done !');
-readline.question();
+displayCountDown();
