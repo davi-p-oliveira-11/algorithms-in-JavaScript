c72b2abc3a0b6780e812368d88c5cd24d1f29ed3
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-04 23:42:25 -0300

    Create custom-counter.js

---
+++ custom-counter.js
@@ -0,0 +1,6 @@
+/*
+ 42) Create an algorithm that asks the user for any 
+ positive integer and displays a countdown up to that value:
+Example: Enter a value: 35
+Countdown: 1 2 3 4 5 6 7 ... 33 34 35 Done !
+*/
