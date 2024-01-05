38fbb81de1f2c3c18bec220838cf27cef79f7cd9
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-04 23:31:40 -0300

    Update count-display2.js

--- count-display2.js
+++ count-display2.js
@@ -2,3 +2,15 @@
  39) Create an algorithm that displays the following countdown on the screen:
      10 9 8 7 6 5 4 3 Done !
 */
+
+const readline = require('readline-sync');
+
+let counter = 10;
+
+while (counter >= 3) {
+  console.log(counter);
+  counter--
+}
+
+console.log('Done !');
+readline.question();
