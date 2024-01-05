2fef3f489c4a8d94a988ab274a3986a56319361c
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-05 01:25:23 -0300

    Update custom-counter.js

--- custom-counter.js
+++ custom-counter.js
@@ -8,14 +8,18 @@ Countdown: 1 2 3 4 5 6 7 ... 33 34 35 Done !
 const prompt = require('prompt-sync')();
 const readline = require('readline-sync');
 
-let number = parseInt(prompt('Enter a positive integer '));
-let counter = 1;
+function displayCountDown() {
+  let number = parseInt(prompt("Enter a positive integer "));
+  let counter = 1;
 
-while (counter <= number) {
-  console.log(counter);
-  counter++
+  while (counter <= number) {
+    console.log(counter);
+    counter++;
+  }
+
+  console.log("Done !");
+  readline.question("Press Enter to Exit ...");
 }
 
-console.log('Done !');
-readline.question();
+displayCountDown();
 
