2602f3d016336a8a23e0bfa13be14eb6b0c223c2
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-04 23:38:13 -0300

    Update count-display4.js

--- count-display4.js
+++ count-display4.js
@@ -2,3 +2,15 @@
  41) Develop a program that displays the following countdown on the screen:
 100 95 90 85 80 ... 0 Done !
 */
+
+const readline = require('readline-sync');
+
+let counter = 100;
+
+while (counter >= 0){
+  console.log(counter);
+  counter -= 5;
+}
+
+console.log('Done !');
+readline.question();
