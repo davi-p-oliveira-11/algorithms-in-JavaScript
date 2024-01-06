1aeb9aff88d085da9c0d42adf50e528dca896126
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-06 14:18:55 -0300

    Update count-display7.js

--- count-display7.js
+++ count-display7.js
@@ -2,3 +2,20 @@
  47) Develop an application that displays on the screen the result of the expression 500 +
 450 + 400 + 350 + 300 + ... + 50 + 0.
 */
+
+const readline = require("readline-sync");
+
+function displayCountDown() {
+  let sum = 0;
+  let counter = 500;
+
+  while (counter != 0) {
+    sum += counter;
+    counter -= 50;
+  }
+
+  console.log(`The sum of 500 + 450 + 400 + 350 + 300 ... + 50 + 0 is: ${sum}`);
+  readline.question('Press Enter to Exit ... ');
+}
+
+displayCountDown();
