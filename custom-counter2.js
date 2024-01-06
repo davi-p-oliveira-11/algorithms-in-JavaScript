8d9dfcdf38f53f1e1b47abe087254dd1cdd480fa
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-06 14:06:04 -0300

    Update custom-counter2.js

--- custom-counter2.js
+++ custom-counter2.js
@@ -6,3 +6,24 @@ Enter the last Value: 10
 Enter the increment: 2
 Count: 3 5 7 9 Done!
 */
+
+const prompt = require("prompt-sync")();
+const readline = require("readline-sync");
+
+function displayCountDown() {
+  let initialValue = parseInt(prompt("Enter the first Value: "));
+  let finalValue = parseInt(prompt("Enter the last Value: "));
+  let increment = parseInt(prompt("Enter the increment: "));
+
+  let counter = initialValue;
+
+  while (counter <= finalValue) {
+    console.log(counter);
+    counter += increment;
+  }
+
+  console.log("Done !");
+  readline.question("Press Enter to Exit ... ");
+}
+
+displayCountDown();
