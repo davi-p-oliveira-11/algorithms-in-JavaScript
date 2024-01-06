6d95655fe81f6bdd37e52ea3ba40d07615c6790a
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-06 14:13:11 -0300

    Update count-display6.js

--- count-display6.js
+++ count-display6.js
@@ -2,3 +2,20 @@
  46) Create a program that calculates and displays on the screen the result of the sum of 6 +
 8 + 10 + 12 + 14 + ... + 98 + 100.
 */
+
+const readline = require("readline-sync");
+
+function displayCountDown() {
+  let sum = 0;
+  let counter = 6;
+
+  while (counter <= 100) {
+    sum += counter;
+    counter += 2;
+  }
+
+  console.log(`The sum of 6 + 8 + 10 + 12 + ... + 98 + 100 is: ${sum}`);
+  readline.question('Press Enter to Exit ... ');
+}
+
+displayCountDown();
