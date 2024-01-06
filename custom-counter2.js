640464eea027a63f9392e63da0071ecd53d64da6
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-06 14:01:37 -0300

    Create custom-counter2.js

---
+++ custom-counter2.js
@@ -0,0 +1,8 @@
+/*
+ 44) Create an algorithm that reads the initial value of the count, the final value, and the
+increment, then displays all the values in the range:
+Example: Enter the first Value: 3
+Enter the last Value: 10
+Enter the increment: 2
+Count: 3 5 7 9 Done!
+*/
