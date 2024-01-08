a9d28ae00822b71ce4a26bd85d876199a5fa832d
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-08 09:29:16 -0300

    Create dont-type-1111.js

---
+++ dont-type-1111.js
@@ -0,0 +1,5 @@
+/*
+56) Create a program that reads several numbers from the keyboard and shows, in the end, the sum of them.
+Note: The program will be interrupted when the number 1111 is entered.
+*/
+
