b9b504a6b317a7b25d1f09140c5974aaecb38435
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-08 09:29:52 -0300

    Update dont-type-1111.js

--- dont-type-1111.js
+++ dont-type-1111.js
@@ -3,3 +3,31 @@
 Note: The program will be interrupted when the number 1111 is entered.
 */
 
+const prompt = require("prompt-sync")();
+const readline = require("readline-sync");
+
+function dontType1111() {
+  let counter = 1;
+  let sum = 0;
+  let number = 0;
+
+  while (counter <= 20) {
+    number = parseInt(prompt("Enter a number: "));
+    if (number === 1111) {
+      console.log(`
+      The number 1111 was entered. End of the program..`);
+      readline.question('Press Enter to Exit ... ');
+      break;
+    }
+    sum += number;
+    counter++;
+  }
+
+  if (number !== 1111) {
+    console.log(`The sum of the entered numbers is equal to ${sum}.`);
+    readline.question();
+  }
+}
+
+dontType1111();
+
