249311b88b22c819ded3f4c94a3bebbdf2fce702
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-01-08 09:20:19 -0300

    Update sum-of-7.js

--- sum-of-7.js
+++ sum-of-7.js
@@ -3,3 +3,21 @@
 of them.
 */
 
+const prompt = require("prompt-sync")();
+const readline = require("readline-sync");
+
+function sumOfSeven() {
+  let counter = 1;
+  let sum = 0;
+
+  while (counter <= 7) {
+    let number = parseInt(prompt("Enter a number: "));
+    sum += number;
+    counter++;
+  }
+
+  console.log(`The sum of the entered numbers is equal to ${sum}`);
+  readline.question('Press Enter to Exit ... ');
+}
+
+sumOfSeven();
